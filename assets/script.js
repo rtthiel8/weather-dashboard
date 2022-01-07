3b5e717529af94d20fc958a493665381cf1425cb
rtthiel8 <93964501+rtthiel8@users.noreply.github.com>
2022-01-06 21:10:32 -0600

    submit button

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city")
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+// get value from input element
+var city = cityInputEl.value.trim();
+
+    if (city) {
+      getCityWeather(city);
+      cityInputEl.value = "";
+    } else {
+      alert("Please enter a valid City");
+    }
+
+  };
+
 var getCityWeather = function(city) {
     // format the api url
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1a9e1beac3e10cf54f1426a2f0ed425b"; 
@@ -10,7 +28,4 @@ var getCityWeather = function(city) {
     });
   };
 
-getCityWeather("Portland");
-
-
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+userFormEl.addEventListener("submit", formSubmitHandler);
